e92efb8268b86f81151caef8d8504e5d8fc57fef
mandrikov@gmail.com <mandrikov@gmail.com@d7541d05-9f01-f6b0-a4b3-e05c6f350eb8>
2010-04-28 13:46:02 +0000

    Add improved dev-util/sonar-bin-2.1_rc2

---
+++ trunk/repository/dev-util/sonar-bin/files/init.sh
@@ -0,0 +1,26 @@
+#!/sbin/runscript
+
+depend() {
+    need net
+    use dns logger
+}
+
+RUN_AS=sonar
+
+checkconfig() {
+    return 0
+}
+
+start() {
+    checkconfig || return 1
+
+    ebegin "Starting ${SVCNAME}"
+    su $RUN_AS -c "/opt/sonar/bin/linux-x86-32/sonar.sh start"
+    eend $?
+}
+
+stop() {
+    ebegin "Stopping ${SVCNAME}"
+    su $RUN_AS -c "/opt/sonar/bin/linux-x86-32/sonar.sh stop"
+    eend $?
+}
